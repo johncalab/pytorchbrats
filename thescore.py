f7e7243ada10603ab54ac1f51ceefaea3e3b1019
jrc <john.robert.calabrese@gmail.com>
2019-06-27 15:26:33 -0500

    minor changes

--- thescore.py
+++ thescore.py
@@ -5,11 +5,9 @@ def iouscore(ypred,y,threshold=0.5):
     'Expects ypred and y to be torch tensors'
     ypred = ypred.detach()
     ypred = torch.sigmoid(ypred)
-    # might have to change this to ypred.cpu().numpy()
     ypred = ypred.cpu().numpy()
     ypred = (ypred > threshold).astype(int)
     
-    #might have to change this to y = y.detach().cpu().numpy().astype(int)
     y = y.detach().cpu().numpy().astype(int)
     
     intersection = np.logical_and(ypred,y)
