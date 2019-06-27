d2c87434c1596c77e2672c04347f1e1f95429409
jrc <john.robert.calabrese@gmail.com>
2019-06-27 12:27:31 -0500

    added iouscore

---
+++ thescore.py
@@ -0,0 +1,15 @@
+def iouscore(ypred,y,threshold=0.5):
+    'Expects ypred and y to be torch tensors'
+    ypred = ypred.detach()
+    ypred = torch.sigmoid(ypred)
+    # might have to change this to ypred.cpu().numpy()
+    ypred = ypred.numpy()
+    ypred = (ypred > threshold).astype(int)
+    
+    #might have to change this to y = y.detach().cpu().numpy().astype(int)
+    y = y.detach().numpy().astype(int)
+    
+    intersection = np.logical_and(ypred,y)
+    union = np.logical_or(ypred,y)
+    score = np.sum(intersection)/np.sum(union)
+    return score
