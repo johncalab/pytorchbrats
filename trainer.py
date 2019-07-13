92e8447f27720587e6b0679400abe96cfe51da3b
johncalab <john.robert.calabrese@gmail.com>
2019-07-13 17:36:58 -0500

    added log files

--- trainer.py
+++ trainer.py
@@ -40,54 +40,68 @@ def gettime():
 
 start_time = gettime()
 logPath = os.path.join('models', start_time + '.log')
-from randoname import randoname
-rn = randoname()
-with open(logPath, 'a') as f:
-    f.write(rn + ' started training.')
+
+def add2log(s,logPath=logPath,display=True):
+    with open(logPath, 'a') as f:
+        if display:
+            print(s)
+        f.write(s)
+    return None
 
 # global variables for later use
 NUM_EPOCHS = args.ne
-print(f"NumEpochs is {NUM_EPOCHS}.")
 BATCH_SIZE = args.bs
-print(f"BatchLength is {BATCH_SIZE}.")
 LR = args.lr
-print(f"Learning rate is {LR}.")
 MOMENTUM = args.momentum
-print(f"Momentum is {MOMENTUM}.")
 RESOLUTION = args.resolution
-print(f"Resolution is {RESOLUTION}.")
 if RESOLUTION == '0':
     dataPath = os.path.join(args.dp, 'numpyDataOG')
 else:
     dataPath = os.path.join(args.dp, 'numpyData'+RESOLUTION)
-print(f"Data path is {dataPath}.")
 SPLIT_FRAC = args.trainsplit
-print(f"TrainValid split is {SPLIT_FRAC}.")
 VERBOSE = args.v
-print(f"Verbose is {VERBOSE}.")
+
+# start logging
+from randoname import randoname
+rn = randoname()
+with open(logPath, 'a') as f:
+    addLog = rn + ' strated training.\n'
+    addLog += f"Model is {args.model}.\n"
+    addLog += f"Resolution is {RESOLUTION}.\n"
+    addLog += f"NumEpochs is {NUM_EPOCHS}.\n"
+    addLog += f"Batch Length is {BATCH_SIZE}.\n"
+    addLog += f"Learning rate is {LR}.\n"
+    addLog += f"Momentum is {MOMENTUM}.\n"
+    addLog += f"Valid/Train ratio is {SPLIT_FRAC}.\n"
+    addLog += f"Data path is {dataPath}.\n"
+    print(addLog)
+    f.write(addLog)
 
 # Get data
 import thedataset
+add2log(f"\n{gettime()} {rn} is loading data.\n")
 datasetname = args.dataset
 datasetClass = getattr(thedataset, datasetname)
 fullDataset = datasetClass(dataPath)
-print(f"There are {len(fullDataset)} images in total.")
+add2log(f"\tThere are {len(fullDataset)} images in total.\n")
 
 # Split into training and validation
 valid_size = int(SPLIT_FRAC * len(fullDataset))
 train_size = len(fullDataset) - valid_size
 train_dataset, valid_dataset = random_split(fullDataset, [train_size, valid_size])
-print(f"There are {len(train_dataset)} training images, and {len(valid_dataset)} validation images.")
+add2log(f"\tThere are {len(train_dataset)} training images, and {len(valid_dataset)} validation images.\n")
 
 # Load data
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
 valid_dataloader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False)
+add2log(f'{gettime()} Data is loaded.\n')
 
 # set device
 device = 'cpu'
 if args.cuda and torch.cuda.is_available():
     device = 'cuda'
-print(f"Device used is {device}.")
+
+print(f"\nDevice used is {device}.")
 device = torch.device(device)
 
 # initialize model
@@ -103,10 +117,11 @@ modelname = args.model
 modelClass = getattr(themodel, modelname)
 model = modelClass()
 model.to(device)
-print(f"Using model {modelname}.")
+add2log(f"\n{gettime()} Model {modelname} was initialized from scratch.\n")
 
 # set optimizer
 optimizer = torch.optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+add2log(f"\tOptimizer SGD with learning rate {LR} and momentum {MOMENTUM}.\n")
 
 # loss function
 import thescore
@@ -116,13 +131,14 @@ else:
     raise KeyError("Only have iou for now.")
 criterion = lossClass()
 # criterion = nn.BCEWithLogitsLoss()
+add2log(f"\tLoss and Score are IoU.\n")
 
 # score function
 from thescore import iou_score
 score_fun = iou_score
 
 # Here starts the training
-print("\nAll right, let's do this.")
+add2log("\nAll right, let's do this.\n")
 
 epochLosses = []
 epochScores = []
@@ -130,12 +146,13 @@ epochTrainScores = []
 
 try:
     for epoch in range(NUM_EPOCHS):
-        print(f'\n-------Epoch {epoch+1}-------')
+        add2log(f'\n-------Epoch {epoch+1}-------\n')
+        add2log(f"{gettime()} {rn} started epoch {epoch+1}.\n")
 
         # training loop----
-        print('\n*Training')
+        add2log('\n\tTraining\n\n')
         model.train()
-        losses = []
+        trainlosses = []
         trainscores = []
         batchloop = tqdm.tqdm(train_dataloader)
         for x,y in batchloop:
@@ -154,26 +171,34 @@ try:
             optimizer.step()
 
             batchloop.set_description(f"Epoch number {epoch+1}, Loss: {loss.item()}")
-            losses.append(loss.item())
+            trainlosses.append(loss.item())
 
             y_pred = y_pred.round()
             y_pred = y_pred.byte()
             y = y.byte()
             score = score_fun(y_pred,y)
-            trainscores.append(score)
+            trainscores.append(score.item())
 
-        avgloss = np.asarray(losses).mean()
-        print(f"The average training loss was {avgloss}.")
+        for loss in trainlosses:
+            add2log(f"\tBatch loss is {loss}.\n", display=False)
+        add2log("\n")
+        for score in trainscores:
+            add2log(f"\tBatch score is {score}.\n",display=False)
+
+
+
+        avgloss = np.asarray(trainlosses).mean()
+        add2log(f"\nThe average training loss was {avgloss}.\n")
         epochLosses.append(avgloss)
 
         avgTrainScore = np.asarray(trainscores).mean()
-        print(f"The average training score was {avgTrainScore}.\n")
+        add2log(f"The average training score was {avgTrainScore}.\n")
         epochTrainScores.append(avgTrainScore)
 
     # -------------
 
         # validation loop----
-        print('\n*Validation')
+        add2log('\n\tTesting\n\n')
         model.eval()
         with torch.no_grad():
             scores = []
@@ -188,12 +213,14 @@ try:
                 y_pred = y_pred.byte()
                 y = y.byte()
                 score = score_fun(y_pred,y)
-                scores.append(score)
+                scores.append(score.item())
             
+            for score in scores:
+                add2log(f"\tBatch score is {score}.\n", display=False)
+
             avgscore = np.asarray(scores).mean()
             epochScores.append(avgscore)
-            print(f"The average score was {avgscore}.")
-
+            add2log(f"\nThe average testing loss was {avgscore}.\n")
 
             # save/overwrite losses and scores
             np.save(os.path.join('models','losses'), epochLosses)
@@ -202,15 +229,17 @@ except KeyboardInterrupt:
     torch.save(model.state_dict(), os.path.join('models','model.pt'))
     print(f"Current model saved as model.pt")
 
-print('\nWhile training, these were the mean losses:\n')
+add2log(f"\n{gettime()} {rn} is done training.\n")
+print('While training, these were the mean losses:\n')
 print(epochLosses)
 print('\nWhile training, these were the mean scores:\n')
 print(epochTrainScores)
 print('\nWhile validating, these were the mean scores:\n')
 print(epochScores)
 
-print("\nI am saving the current model now.")
-torch.save(model.state_dict(), os.path.join('models','model.pt'))
+add2log(f"Saving model.\n")
+modelPath = os.path.join('models', start_time + '_model' + '.pt')
+torch.save(model.state_dict(), modelPath)
 
 if args.plot:
     import matplotlib.pyplot as plt
@@ -226,8 +255,11 @@ if args.plot:
     plt.plot(epochTrainScores,'r', label='Training scores')
     plt.legend()
 
+    plotPath = os.path.join('models', start_time + '_plot' + '.png')
+    plt.savefig(plotPath)
     plt.show()
-    plt.savefig(os.path.join('models','plot.png'))
+    
+    
 
 """
 To reload models: 
