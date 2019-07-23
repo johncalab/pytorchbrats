113d65c4446a2ec9fecbd8d434185f9abfc43a77
johncalab <john.robert.calabrese@gmail.com>
2019-07-23 14:13:28 -0500

    nonempty

--- themodel.py
+++ themodel.py
@@ -133,7 +133,7 @@ class Small3dUcat(nn.Module):
 
 # small 3d u-net with addition skip connection
 class Small3dUadd(nn.Module):
-    def __init__(self,input_channels=4,num_filters=16):
+    def __init__(self,input_channels=4,num_filters=32):
         super(Small3dUadd,self).__init__()
         
         self.input_channels = input_channels
@@ -186,7 +186,7 @@ class Small3dUadd(nn.Module):
 
 # 3d UU-net
 class UU3d(nn.Module):
-    def __init__(self,input_channels=4, num_filters=32):
+    def __init__(self,input_channels=4, num_filters=16):
         super(UU3d,self,).__init__()
 
         n = num_filters
@@ -308,7 +308,7 @@ class UU3d(nn.Module):
 # include picture of architecture
 # small 3d u-net with addition skip connection
 class Unet3d(nn.Module):
-    def __init__(self,input_channels=4,num_filters=32):
+    def __init__(self,input_channels=4,num_filters=16):
         super(Unet3d,self).__init__()
         
         # Structure:
